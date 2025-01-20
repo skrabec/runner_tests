6ac0ac598efc100a2d61d349034714c4542de87e
Nikolaj Skrabec <skrabets@gmail.com>
2025-01-20 20:00:09 +0100

    Update runner.groovy

--- jenkins/runner.groovy
+++ jenkins/runner.groovy
@@ -1,32 +1,19 @@
-// Define the function at the top level
-def prepare_yaml_config(config) {
-    def yaml_config = readYaml text: config
-    yaml_config.each { k, v ->
-        env[k] = v.toString()
-    }
-}
-
-// Main pipeline
 timeout(time: 10, unit: 'MINUTES') {
     node('maven') {
+
+        dir('jenkins') {  
+            utils = load './utils.groovy'
+        }
+
+        utils.prepare_yaml_config()
+
         def jobs = [:]
         
-        // Call the function with proper parameter
-        prepare_yaml_config(env.CONFIG)
-        
-        def testTypes = env.TESTS instanceof String ? env.TESTS.split(',') : env.TESTS
-        
-        testTypes.each { test_type ->
+        for(def test_type: env.TESTS) {
             jobs["${test_type}-tests"] = {
                 stage("Running ${test_type} tests") {
-                    def parameters = [
-                        [$class: 'StringParameterValue', name: 'SRCSPEC', value: env.SRCSPEC ?: ''],
-                        [$class: 'StringParameterValue', name: 'CONFIG', value: env.CONFIG ?: '']
-                    ]
-                    
-                    build job: "${test_type}-tests",
-                          parameters: parameters,
-                          propagate: false
+                    def parameters = ["SRCSPEC", "CONFIG"]
+                    build job: "${test_type}-tests", parameters: parameters, propagate: false
                 }
             }
         }
