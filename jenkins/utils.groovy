71e86289d3c650a6a8f6714e409078b19c6ec043
Nikolaj Skrabec <skrabets@gmail.com>
2025-01-20 20:03:34 +0100

    Update utils.groovy

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -1,9 +1,9 @@
 def prepare_yaml_config() {
-    def config = readYaml text: $CONFIG
-
-    config.each( k, v -> {
-        env.setProperty(k,v)
-    })
+    def config = readYaml text: env.CONFIG
+    
+    config.each { k, v ->  
+        env[k] = v.toString()
+    }
 }
 
 return this
