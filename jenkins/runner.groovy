5801c57eb714b9b988eb2d33bcbe54df5a7a9171
Nikolaj Skrabec <skrabets@gmail.com>
2025-01-20 19:54:06 +0100

    Update runner.groovy

--- jenkins/runner.groovy
+++ jenkins/runner.groovy
@@ -1,26 +1,37 @@
 timeout(time: 10, unit: 'MINUTES') {
     node('maven') {
-        
-
         def prepare_yaml_config() {
-            def config = readYaml text: $CONFIG
+            // Parse YAML config to map
+            def config = readYaml text: env.CONFIG
 
-            config.each( k, v -> {
-                env.setProperty(k,v)
-            })
+            // Iterate using proper Groovy map syntax and environment variable setting
+            config.each { k, v ->
+                env[k] = v.toString()
+            }
         }
 
         def jobs = [:]
         
-        for(def test_type: env.TESTS) {
+        // Need to ensure env.TESTS is properly initialized and split if it's a string
+        def testTypes = env.TESTS instanceof String ? env.TESTS.split(',') : env.TESTS
+        
+        testTypes.each { test_type ->
+            // Create proper parameter list with values
             jobs["${test_type}-tests"] = {
                 stage("Running ${test_type} tests") {
-                    def parameters = ["SRCSPEC", "CONFIG"]
-                    build job: "${test_type}-tests", parameters: parameters, propagate: false
+                    def parameters = [
+                        [$class: 'StringParameterValue', name: 'SRCSPEC', value: env.SRCSPEC ?: ''],
+                        [$class: 'StringParameterValue', name: 'CONFIG', value: env.CONFIG ?: '']
+                    ]
+                    
+                    build job: "${test_type}-tests",
+                          parameters: parameters,
+                          propagate: false
                 }
             }
         }
 
+        prepare_yaml_config()
         parallel jobs
     }
 }
