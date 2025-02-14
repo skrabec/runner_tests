d73cb0f6d775c049d323fd26e96f32a1827aca40
Nikolaj Skrabec <skrabets@gmail.com>
2025-02-14 14:43:19 +0100

    edit runner

--- jenkins/runner.groovy
+++ jenkins/runner.groovy
@@ -1,21 +1,23 @@
 timeout(time: 10, unit: 'MINUTES') {
     node('maven') {
-        checkout scm
 
 
-        dir('jenkins') {  
-            utils = load './utils.groovy'
-        }
-
+        utils = load './utils.groovy'
         utils.prepare_yaml_config()
 
         def jobs = [:]
-        
         for(def test_type: env.TESTS) {
-            jobs["${test_type}-tests"] = {
-                stage("Running ${test_type} tests") {
-                    def parameters = ["SRCSPEC", "CONFIG"]
-                    build job: "${test_type}-tests", parameters: parameters, propagate: false
+        
+            jobs[test_type] = node ('maven') {
+                stage("Running $test_type tests") {
+
+                    def parameters = [
+                        "SRCSPEC", 
+                        "CONFIG"
+                        ]
+
+                    build (job: "$test_type-tests", 
+                    parameters: parameters, propagate: false
                 }
             }
         }
